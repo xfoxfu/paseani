68a8071e854230d961e4de6afd4ba99103ae0558
Yuze Fu <i@xfox.me>
2024-03-03 21:32:15 +0900

    refactor: use expressjs

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,18 @@ import { GJYParser } from "./parser/GJYParser.js";
 import { LilithOrAniParser } from "./parser/LilithOrAniParser.js";
 import { PrefixMatchParser } from "./parser/PrefixMatchParser.js";
 import { TagNormalizer } from "./parser/TagNormalizer.js";
-import { Parser, chainedParse } from "./parser/index.js";
-import Router from "@koa/router";
-import Koa from "koa";
-import serve from "koa-static";
+import { chainedParse } from "./parser/index.js";
+import express from "express";
 
-const app = new Koa();
-const router = new Router();
+const parsers = [
+  new GJYParser(),
+  new LilithOrAniParser(),
+  new PrefixMatchParser(),
+  new TagNormalizer(),
+  new LinkEnricher(),
+];
 
-const buildParsers = () => {
-  parsers = [
-    new GJYParser(),
-    new LilithOrAniParser(),
-    new PrefixMatchParser(),
-    new TagNormalizer(),
-    new LinkEnricher(),
-  ];
-};
-const initParsers = async () => {
+void (async () => {
   await GlobalDatabase.init();
   for (const parser of parsers) {
     try {
@@ -32,36 +26,40 @@ const initParsers = async () => {
       log.error(e);
     }
   }
-};
+})();
 
-let parsers: Parser[] = [];
-buildParsers();
-void initParsers();
+const app = express();
 
-router.get("/info", (ctx) => {
-  const name = ctx.query["name"] as string;
+app.get("/info", (req, res) => {
+  const name = req.query["name"] as string;
   if (typeof name !== "string") {
-    ctx.status = 400;
-    ctx.body = { error: "Query param `name` must be a string." };
-  }
-  const result = chainedParse(parsers, name);
-  ctx.body = result;
-});
-
-router.post("/internal/database/update", async (ctx) => {
-  if ((ctx.query["token"] ?? "INVALID_TOKEN") !== process.env["PASEANI_ADMIN_TOKEN"]) {
-    ctx.status = 403;
+    res.status(400).send({ error: "Query param `name` must be a string." });
     return;
   }
-  await Promise.all(
-    GlobalDatabase.rawDatabases.map(async (d) => {
-      console.log(d.name);
-      await d.update();
-    }),
-  );
-  await GlobalDatabase.init();
-  ctx.body = { status: "ok" };
+  const result = chainedParse(parsers, name);
+  res.status(200).send(result);
 });
 
-app.use(router.routes()).use(router.allowedMethods()).use(serve("public", {})).listen(3000);
+app.post("/internal/database/update", (req, res) => {
+  (async () => {
+    if ((req.query["token"] ?? "INVALID_TOKEN") !== process.env["PASEANI_ADMIN_TOKEN"]) {
+      return { status: "invalid_token" };
+    }
+    await Promise.all(
+      GlobalDatabase.rawDatabases.map(async (d) => {
+        console.log(d.name);
+        await d.update();
+      }),
+    );
+    await GlobalDatabase.init();
+    return { status: "ok" };
+  })()
+    .then((data) => res.status(200).send(data))
+    .catch((err: Error & { status?: number }) => res.status(err.status ?? 500).send({ message: err.message }));
+});
+
+app.use(express.static("public"));
+
+app.listen(3000);
+
 log.info("paseani listening on 3000");
