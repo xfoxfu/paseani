a9a71a5f0835520740084fd60b01d9596f3d4542
Yuze Fu <i@xfox.me>
2024-03-08 14:03:26 +0900

    feat: qa script based on parser

--- bin/qa.ts
+++ bin/qa.ts
@@ -1,9 +1,17 @@
-import { BangumiParser, GJYParser, LilithOrAniParser, Parser } from "../src/lib.js";
-import { chainedParse } from "../src/parser/index.js";
+import { GlobalDatabase } from "../src/database/index.js";
+import { GJYParser, LilithOrAniParser, LoliHouseBParser, LoliHouseParser } from "../src/lib.js";
+import { ResultBuilder, TagType, chainedParse } from "../src/parser/index.js";
 import { readFile, writeFile } from "fs/promises";
 import { tqdm } from "ts-tqdm";
 
-const parsers: Parser[] = [new GJYParser(), new LilithOrAniParser(), new BangumiParser()];
+// const parsers: Parser[] = [new GJYParser(), new LilithOrAniParser(), new LoliHouseParser()];
+const parser = (() => {
+  if (process.argv[2] === "GJYParser") return new GJYParser();
+  if (process.argv[2] === "LilithOrAniParser") return new LilithOrAniParser();
+  if (process.argv[2] === "LoliHouseParser") return new LoliHouseParser();
+  if (process.argv[2] === "LoliHouseBParser") return new LoliHouseBParser();
+  throw new Error(`invalid parser '${process.argv[2]}'`);
+})();
 
 let ok = 0;
 let errored = 0;
@@ -11,24 +19,25 @@ let unparsed = 0;
 
 let csv = "";
 const main = async () => {
-  await Promise.all(parsers.map((p) => p.init()));
+  await GlobalDatabase.init();
   const titles = (await readFile("data/qa/titles.csv")).toString("utf8");
   for (const title of tqdm(titles.split("\n"))) {
-    const res = chainedParse(parsers, title as string);
+    if (!parser.canParse(title as string, new ResultBuilder())) continue;
+    const res = chainedParse([parser], title as string);
     let status = "ok";
     if (res.errors.length > 0) {
       errored += 1;
       status = "errored";
-    } else if (res.title.length === 0) {
+    } else if (!res.tags.some((t) => t.type === TagType.title)) {
       unparsed += 1;
       status = "unparsed";
     } else {
       ok += 1;
     }
-    csv += `${title},${status},${res.errors.join(":")}\n`;
+    csv += `${title},${status},${res.errors.map((x) => x.message).join(":")}\n`;
   }
   console.log(`ok=${ok} unparsed=${unparsed} errored=${errored}`);
-  await writeFile("data/qa/result.csv", csv);
+  await writeFile("data/qa/result-loli.csv", csv);
 };
 
 main().catch(console.error);
